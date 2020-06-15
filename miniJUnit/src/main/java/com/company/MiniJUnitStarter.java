53f72dadc2ab6af67a1e563b3c6ef2cd1ae01697
Maria <maria.rozhkova@bravedevelopers.com>
2020-06-15 12:36:58 +0000

    Adding test for miniJUnit

--- miniJUnit/src/main/java/com/company/MiniJUnitStarter.java
+++ miniJUnit/src/main/java/com/company/MiniJUnitStarter.java
@@ -16,55 +16,71 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class MiniJUnitStarter {
-    public static void main( String[] args ) throws InvocationTargetException, IllegalAccessException, InstantiationException, MalformedURLException {
-        // set -ea flag to enable assertions
-        ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
 
-        // add settings to scan test classes
-        URL testClassesURL = Paths.get("target/test-classes").toUri().toURL();
-        URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{testClassesURL},
-                ClasspathHelper.staticClassLoader());
+    private static List<String> failedMethodsNames;
 
-        List<ClassLoader> classLoadersList = new LinkedList<>();
-        classLoadersList.add(ClasspathHelper.contextClassLoader());
-        classLoadersList.add(ClasspathHelper.staticClassLoader());
-        classLoadersList.add(classLoader);
+    public static void main( String[] args ) throws InvocationTargetException, IllegalAccessException, InstantiationException, MalformedURLException, ClassNotFoundException {
+        if (args.length == 0)
+            throw new RuntimeException("Not enough arguments");
 
-        Reflections reflections = new Reflections(new ConfigurationBuilder()
-                .setScanners(new SubTypesScanner(false), new ResourcesScanner())
-                .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
-                .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix("com.company"))));
-        Set<Class<?>> allClasses = reflections.getSubTypesOf(Object.class);
+        failedMethodsNames = new ArrayList<>();
 
-        List<String> failedMethodsNames = new ArrayList<>();
+        if (args.length == 1) {
 
-        for (Class clazz : allClasses) {
-            int failedTests = 0;
-            int successfulTest = 0;
-            for (Method method : clazz.getDeclaredMethods()) {
-                if (method.getAnnotation(Test.class) != null) {
-                    try {
-                        method.invoke(clazz.newInstance());
-                        successfulTest++;
-                    } catch (InvocationTargetException e) {
-                        if (e.getCause() instanceof AssertionError) {
-                            failedTests++;
-                            failedMethodsNames.add(clazz.getSimpleName() + "." + method.getName());
-                        }
-                        else
-                            throw e;
-                    }
-                }
+            // set -ea flag to enable assertions
+            ClassLoader.getSystemClassLoader().setDefaultAssertionStatus(true);
+
+            // add settings to scan test classes
+            URL testClassesURL = Paths.get("target/test-classes").toUri().toURL();
+            URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{testClassesURL},
+                    ClasspathHelper.staticClassLoader());
+
+            List<ClassLoader> classLoadersList = new LinkedList<>();
+            classLoadersList.add(ClasspathHelper.contextClassLoader());
+            classLoadersList.add(ClasspathHelper.staticClassLoader());
+            classLoadersList.add(classLoader);
+
+            Reflections reflections = new Reflections(new ConfigurationBuilder()
+                    .setScanners(new SubTypesScanner(false), new ResourcesScanner())
+                    .setUrls(ClasspathHelper.forClassLoader(classLoadersList.toArray(new ClassLoader[0])))
+                    .filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(args[0]))));
+            Set<Class<?>> allClasses = reflections.getSubTypesOf(Object.class);
+
+            for (Class clazz : allClasses) {
+                testClasses(clazz);
             }
 
-            if (successfulTest != 0 || failedTests != 0)
-                System.out.println(ConsoleColors.GREEN + "Successful: " + successfulTest + ConsoleColors.RED + " Failed: " + failedTests);
+        } else if (args.length == 2) {
+            testClasses(Class.forName(args[0] + "." + args[1]));
         }
 
         if (!failedMethodsNames.isEmpty()) {
             System.out.println("\nFailed tests:");
-            for (String s: failedMethodsNames)
+            for (String s : failedMethodsNames)
                 System.out.println(s);
         }
+    }
+
+    private static void testClasses(Class clazz) throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        int failedTests = 0;
+        int successfulTest = 0;
+        for (Method method : clazz.getDeclaredMethods()) {
+            if (method.getAnnotation(Test.class) != null) {
+                try {
+                    method.invoke(clazz.newInstance());
+                    successfulTest++;
+                } catch (InvocationTargetException e) {
+                    if (e.getCause() instanceof AssertionError) {
+                        failedTests++;
+                        failedMethodsNames.add(clazz.getSimpleName() + "." + method.getName());
+                    }
+                    else
+                        throw e;
+                }
+            }
+        }
+
+        if (successfulTest != 0 || failedTests != 0)
+            System.out.println(ConsoleColors.GREEN + "Successful: " + successfulTest + ConsoleColors.RED + " Failed: " + failedTests);
     }
 }
--- miniJUnit/src/test/java/com/company/AppTest.java
+++ miniJUnit/src/test/java/com/company/AppTest.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
@@ -9,13 +10,18 @@ import java.net.MalformedURLException;
 
 import static junit.framework.TestCase.assertEquals;
 
-public class AppTest 
-{
-    @Test
-    public void testMiniJUnit() throws IllegalAccessException, InstantiationException, InvocationTargetException, MalformedURLException {
-        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+public class AppTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
-        MiniJUnitStarter.main(null);
+    }
+
+    @Test
+    public void testMiniJUnit() throws IllegalAccessException, InstantiationException, InvocationTargetException, MalformedURLException, ClassNotFoundException {
+        MiniJUnitStarter.main(new String[] {"com.company"});
         assertEquals("\u001B[0;32mSuccessful: 1\u001B[0;31m Failed: 2\n" +
                 "\u001B[0;32mSuccessful: 2\u001B[0;31m Failed: 1\n" +
                 "\n" +
@@ -23,5 +29,24 @@ public class AppTest
                 "UnitTests2.unitTest2\n" +
                 "UnitTests2.unitTest3\n" +
                 "UnitTests.unitTest2\n", outContent.toString());
+    }
+
+    @Test
+    public void testMiniJUnit1() throws IllegalAccessException, InstantiationException, InvocationTargetException, MalformedURLException, ClassNotFoundException {
+        MiniJUnitStarter.main(new String[] {"com.company", "UnitTests"});
+        assertEquals("\u001B[0;32mSuccessful: 2\u001B[0;31m Failed: 1\n" +
+                "\n" +
+                "Failed tests:\n" +
+                "UnitTests.unitTest2\n", outContent.toString());
+    }
+
+    @Test
+    public void testMiniJUnit2() throws IllegalAccessException, InstantiationException, InvocationTargetException, MalformedURLException, ClassNotFoundException {
+        MiniJUnitStarter.main(new String[] {"com.company", "UnitTests2"});
+        assertEquals("\u001B[0;32mSuccessful: 1\u001B[0;31m Failed: 2\n" +
+                "\n" +
+                "Failed tests:\n" +
+                "UnitTests2.unitTest2\n" +
+                "UnitTests2.unitTest3\n", outContent.toString());
     }
 }
